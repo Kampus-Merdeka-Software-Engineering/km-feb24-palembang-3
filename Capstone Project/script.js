61495715e4bb99a7203a9a66406cb012a411582f
unknown <imranzulkarnaen46@gmail.com>
2024-05-19 21:40:05 +0700

    Style Change

--- Capstone Project/script.js
+++ Capstone Project/script.js
@@ -1,72 +1,83 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetchDataAndDrawCharts();
-  });
-  
-  async function fetchDataAndDrawCharts() {
-    try {
-      const response = await fetch('./data/dataset.json');
-      const data = await response.json();
-  
-      // Sorting data based on RQty (Product Terjual) and Transaction (Total Transaksi)
-      const sortedByRQty = data.sort((a, b) => b.RQty - a.RQty);
-      const sortedByTransaction = data.sort((a, b) => b.Transaction - a.Transaction);
-  
-      // Selecting top 5 products
-      const top5ProductsRQty = sortedByRQty.slice(0, 5);
-      const top5ProductsTransaction = sortedByTransaction.slice(0, 5);
-  
-      const labels1 = top5ProductsRQty.map(item => item.Product);
-      const data1 = top5ProductsRQty.map(item => item.RQty);
-  
-      const labels2 = top5ProductsTransaction.map(item => item.Product);
-      const data2 = top5ProductsTransaction.map(item => item.Transaction);
-  
-      drawChart('myChart1', 'Product Terjual', labels1, data1, 'rgba(255, 99, 132, 0.2)', 'rgba(255, 99, 132, 1)');
-      drawChart('myChart2', 'Total Transaksi', labels2, data2, 'rgba(54, 162, 235, 0.2)', 'rgba(54, 162, 235, 1)');
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+  fetchDataAndDrawCharts();
+});
+
+async function fetchDataAndDrawCharts() {
+  try {
+    const response = await fetch('./data/dataset.json');
+    const data = await response.json();
+
+    const top5Products = data.reduce((acc, curr) => {
+      const product = curr.Product;
+      if (!acc[product]) {
+        acc[product] = { product, total: 0 };
+      }
+      acc[product].total += curr.RQty;
+      return acc;
+    }, {});
+
+    const sortedTop5Products = Object.values(top5Products).sort((a, b) => b.total - a.total);
+    const top5ProductsRQty = sortedTop5Products.slice(0, 5).map(item => item.total);
+    const top5ProductsLabels = sortedTop5Products.slice(0, 5).map(item => item.product);
+
+    const top5Categories = data.reduce((acc, curr) => {
+      const category = curr.Category;
+      if (!acc[category]) {
+        acc[category] = { category, total: 0 };
+      }
+      acc[category].total += curr.RQty;
+      return acc;
+    }, {});
+
+    const sortedTop5Categories= Object.values(top5Categories).sort((a, b) => b.total - a.total);
+    const top5CategoriesRQty = sortedTop5Categories.slice(0, 5).map(item => item.total);
+    const top5CategoriesLabels = sortedTop5Categories.slice(0, 5).map(item => item.category);
+
+    drawChart('myChart1', 'Top 5 Produk Terjual', top5ProductsLabels, top5ProductsRQty, 'rgba(255, 99, 132, 0.2)', 'rgba(255, 99, 132, 1)');
+    drawChart('myChart2', 'Top 5 Kategori Produk', top5CategoriesLabels, top5CategoriesRQty, 'rgba(54, 162, 235, 0.2)', 'rgba(54, 162, 235, 1)');
+  } catch (error) {
+    console.error('Error fetching data:', error);
   }
-  
-  function drawChart(canvasId, label, labels, data, backgroundColor, borderColor) {
-    const ctx = document.getElementById(canvasId).getContext('2d');
-  
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: label,
-          data: data,
-          backgroundColor: backgroundColor,
-          borderColor: borderColor,
-          borderWidth: 1
-        }]
+}
+
+function drawChart(canvasId, label, labels, data, backgroundColor, borderColor) {
+  const ctx = document.getElementById(canvasId).getContext('2d');
+
+  new Chart(ctx, {
+   type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: label,
+        data: data,
+        backgroundColor: backgroundColor,
+        borderColor: borderColor,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
       },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
+      plugins: {
+        legend: {
+          display: false
         },
-        plugins: {
-          legend: {
-            display: false
-          },
-          tooltip: {
-            callbacks: {
-              label: function(context) {
-                var label = context.dataset.label || '';
-                if (label) {
-                  label += ': ';
-                }
-                label += context.parsed.y + ' (' + data[context.dataIndex] + ')';
-                return label;
+        tooltip: {
+          callbacks: {
+            label: function(context) {
+              var label = context.dataset.label || '';
+              if (label) {
+                label += ': ';
               }
+              label += context.parsed.y + ' (' + data[context.dataIndex] + ')';
+              return label;
             }
           }
         }
       }
-    });
-  }
-  
+    }
+  });
+}
